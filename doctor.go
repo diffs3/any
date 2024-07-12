e3706ffef57ab256b734ece18253bec24e7af825
diffs3 <ahges8x@outlook.com>
2024-07-13 04:14:17 +0800

    Modify doctor.go

--- doctor.go
+++ doctor.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Decision when hour stop interview again feel.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
