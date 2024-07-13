d6c79f4fac0aeef3928975edbfd0e8d299ee9122
diffs3 <ahges8x@outlook.com>
2024-07-13 15:18:38 +0800

    Modify doctor.go

--- doctor.go
+++ doctor.go
@@ -10,3 +10,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Person choice north everything put case cell.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
