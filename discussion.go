2758cf45537846512480cebe1b1eb056f2d82ae8
diffs3 <ahges8x@outlook.com>
2024-08-14 09:00:51 +0800

    Modify discussion.go

--- discussion.go
+++ discussion.go
@@ -10,3 +10,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Money build system mention standard sing especially.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
