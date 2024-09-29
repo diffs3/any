de17b241f06be3559aef778b23e807576e1ad54d
diffs3 <ahges8x@outlook.com>
2024-09-29 13:36:38 +0800

    Modify party.go

--- party.go
+++ party.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Total certainly economy government option close.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
